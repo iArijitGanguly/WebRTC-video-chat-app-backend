d8adb6945ffaaa26c8927e10ca37d7a2c4dd9e77
iArijitGanguly <arijeetGanguly63@gmail.com>
2024-05-08 22:30:13 +0530

    basic web socket connection is set up

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import cors from 'cors';
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+import serverConfig from './configs/serverConfig';
+
+const { PORT } = serverConfig;
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('New User Connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server Started at PORT: ${PORT}`);
+});
