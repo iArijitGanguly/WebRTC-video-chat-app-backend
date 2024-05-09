d68b4855ba19a5f599e87201965039bfcf0df944
iArijitGanguly <arijeetGanguly63@gmail.com>
2024-05-09 20:34:13 +0530

    Room creation logic wuth user joinning the room and their record of joining being stored added

--- src/handlers/roomHandler.ts
+++ src/handlers/roomHandler.ts
@@ -1,16 +1,31 @@
 import { Socket } from 'socket.io';
 import { v4 as UUIdv4 } from 'uuid';
 
+import { IRoomParams } from '../interfaces/IRoomParams';
+
+const rooms: Record<string, string[]> = {};
+
 const roomHandler = (socket: Socket) => {
     const createRoom = () => {
         const roomId = UUIdv4();
         socket.join(roomId);
+        rooms[roomId] = [];
         socket.emit('room-created', { roomId });
         console.log('Room created at : ', roomId);
     };
 
-    const joinedRoom = ({ roomId }: { roomId: string }) => {
-        console.log('New user has joined room', roomId);
+    const joinedRoom = ({ roomId, peerId }: IRoomParams) => {
+        if(rooms[roomId]) {
+            console.log('New user has joined room with ', roomId, 'with peerId: ', peerId);
+            rooms[roomId].push(peerId);
+            socket.join(roomId);
+
+            //below event is for logging purpose
+            socket.emit('get-users', {
+                roomId,
+                participants: rooms[roomId]
+            });
+        }
     };
 
     socket.on('create-room', createRoom);
